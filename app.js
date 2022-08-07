62552e9cffc260f05ec0d62c947ca41141281612
dejanV91 <dejanvukoicic91@gmail.com>
2022-08-07 21:55:45 +0200

    initial comit

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const url = 'https://icanhazdadjoke.com/';
 const btn = document.querySelector('.btn');
 const content = document.querySelector(".cont");
 
+btn.addEventListener("click", () => {
+    request();
+});
+
+
+const request = async () => {
+    let conect = await fetch(url,{
+        headers: {Accept: "application/json"}
+    });
+
+    let data = await conect.json();
+    content.textContent = data.joke;
+}
+
+request();
